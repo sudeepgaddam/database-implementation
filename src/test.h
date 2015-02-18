019d8e94c1cf1cbe695c75ae72846fffeaa15810
Sudeep Reddy Gaddam <sudeepgaddam@gmail.com>
2015-02-18 18:25:55 -0500

    Changed test.cc and test.h required for phase 2

--- src/test.h
+++ src/test.h
@@ -1,17 +1,33 @@
 #ifndef TEST_H
 #define TEST_H
 #include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
+#include <stdlib.h>
+#include "Pipe.h"
 #include "DBFile.h"
 #include "Record.h"
+
 using namespace std;
+
+// make sure that the information below is correct
+
+char *catalog_path = "data/catalog"; 
+char *tpch_dir ="tpc-h/"; // dir where dbgen tpch files (extension *.tbl) can be found
+char *dbfile_dir = "bin/"; 
+
 
 extern "C" {
 	int yyparse(void);   // defined in y.tab.c
 }
 
 extern struct AndList *final;
+
+typedef struct {
+	Pipe *pipe;
+	OrderMaker *order;
+	bool print;
+	bool write;
+}testutil;
 
 class relation {
 
@@ -25,9 +41,6 @@ public:
 		rname (_name), rschema (_schema), prefix (_prefix) {
 		sprintf (rpath, "%s%s.bin", prefix, rname);
 	}
-	~relation(){
-		delete rschema;
-	}
 	char* name () { return rname; }
 	char* path () { return rpath; }
 	Schema* schema () { return rschema;}
@@ -40,12 +53,29 @@ public:
 	void get_cnf (CNF &cnf_pred, Record &literal) {
 		cout << " Enter CNF predicate (when done press ctrl-D):\n\t";
   		if (yyparse() != 0) {
-			std::cout << "Can't parse your CNF.\n";
+			cout << "Can't parse your CNF.\n";
 			exit (1);
 		}
 		cnf_pred.GrowFromParseTree (final, schema (), literal); // constructs CNF predicate
 	}
+	void get_sort_order (OrderMaker &sortorder) {
+		cout << "\n specify sort ordering (when done press ctrl-D):\n\t ";
+  		if (yyparse() != 0) {
+			cout << "Can't parse your sort CNF.\n";
+			exit (1);
+		}
+		cout << " \n";
+		Record literal;
+		CNF sort_pred;
+		sort_pred.GrowFromParseTree (final, schema (), literal); // constructs CNF predicate
+		OrderMaker dummy;
+		sort_pred.GetSortOrders (sortorder, dummy);
+	}
 };
+
+
+relation *rel;
+
 
 char *supplier = "supplier"; 
 char *partsupp = "partsupp"; 
@@ -58,7 +88,7 @@ char *lineitem = "lineitem";
 
 relation *s, *p, *ps, *n, *li, *r, *o, *c;
 
-void setup (char *catalog_path, char *dbfile_dir, char *tpch_dir) {
+void setup () {
 	cout << " \n** IMPORTANT: MAKE SURE THE INFORMATION BELOW IS CORRECT **\n";
 	cout << " catalog location: \t" << catalog_path << endl;
 	cout << " tpch files dir: \t" << tpch_dir << endl;
@@ -75,17 +105,8 @@ void setup (char *catalog_path, char *dbfile_dir, char *tpch_dir) {
 	c = new relation (customer, new Schema (catalog_path, customer), dbfile_dir);
 }
 
-
-
 void cleanup () {
-	delete s;
-	delete p;
-	delete ps;
-	delete n;
-	delete li;
-	delete r;
-	delete o;
-	delete c;
+	delete s, p, ps, n, li, r, o, c;
 }
 
 #endif
